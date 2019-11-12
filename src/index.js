b04d90e14eab04990697cf7e2c2aa2b9c8de93c0
Misha <Lampard0307@yandex.ru>
2019-11-13 02:45:38 +0300

    Push broken app to make history of commits

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import reducer from './reducers'
+import LocalWeather from './localWeather';
+import Favorites from './favorites';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <LocalWeather />,
+    document.getElementById('geo')
+);
+
+
+let persistedState = localStorage.getItem('state') ? JSON.parse(localStorage.getItem('state')) : [];
+
+let store = createStore(reducer, persistedState);
+
+const render = () => ReactDOM.render(
+    <Favorites
+        items={store.getState()}
+        onAdd={(name) => store.dispatch({ type: 'ADD',  name: name})}
+        onDelete={(name) => store.dispatch({ type: 'DELETE', name: name})}
+    />,
+    document.getElementById('items')
+);
+
+render();
+store.subscribe(() => {
+    localStorage.setItem('state', JSON.stringify(store.getState()));
+});
+
